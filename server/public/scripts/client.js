d5030a53cf5da19c02dad989d203ca9972e1c8b9
cchelton <59346941+cchelton@users.noreply.github.com>
2020-04-04 16:06:13 -0500

    added ajax post to add task to database

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -35,7 +35,26 @@ function getTasksFromDB() {
       renderTasks();
     })
     .catch((err) => {
-      console.log(`ERROR GETTING TASKS FROM DB: ${err}`);
+      console.log(`ERROR GETTING TASKS: ${err}`);
+    });
+}
+
+function addTaskToDB() {
+  const testTask = {
+    task: "DOM TEST TASK",
+  };
+
+  $.ajax({
+    method: "POST",
+    url: "/tasks",
+    data: testTask,
+  })
+    .then((response) => {
+      console.log(response);
+      getTasksFromDB();
+    })
+    .catch((err) => {
+      console.log(`ERROR ADDING TASK: ${err}`);
     });
 }
 
