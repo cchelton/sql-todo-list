40a61eb9ea476a073cb481eae51b5679ab81f8c4
cchelton <59346941+cchelton@users.noreply.github.com>
2020-04-04 13:42:57 -0500

    POST route for inserting new task.
    POST route for /tasks inserts a new task to the database, defaulting completed to false.

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../modules/pool");
 
+// GET  --gets tasks from DB. Has url param to sort
 router.get("/:sortBy?", (req, res) => {
   const sortBy = req.params.sortBy;
   let queryText = "";
@@ -16,10 +17,30 @@ router.get("/:sortBy?", (req, res) => {
         res.send(dbRows);
       })
       .catch((err) => {
-        console.log(`ERROR: ${err}`);
+        console.log(`GET ERROR: ${err}`);
         res.sendStatus(500);
       });
   }
+});
+
+//  POST  --inserts new task to DB
+router.post("/", (req, res) => {
+  const taskData = req.body;
+  console.log(taskData);
+
+  const queryText = `INSERT INTO "tasks" ("task", "completed")
+    VALUES ($1, FALSE);`;
+
+  pool
+    .query(queryText, [taskData.task])
+    .then((dbRes) => {
+      console.log(dbRes);
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log(`POST ERROR: ${err}`);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
