cd7db63b757d9801603807ff88360d6bb35e6ca5
cchelton <59346941+cchelton@users.noreply.github.com>
2020-04-04 15:51:50 -0500

    get route sorts by passing partial SQL query as url params

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -2,25 +2,27 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../modules/pool");
 
-// GET      --gets tasks from DB. Has url param to sort
+// GET      --gets tasks from DB. sortBy = SQL query string `ORDER BY [order]`
 router.get("/:sortBy?", (req, res) => {
   const sortBy = req.params.sortBy;
   let queryText = "";
 
   if (!sortBy) {
     queryText = `SELECT * FROM "tasks" ORDER BY "id" ASC;`;
-    pool
-      .query(queryText)
-      .then((dbRes) => {
-        const dbRows = dbRes.rows;
-        console.table(dbRows);
-        res.send(dbRows);
-      })
-      .catch((err) => {
-        console.log(`GET ERROR: ${err}`);
-        res.sendStatus(500);
-      });
+  } else {
+    queryText = `SELECT * FROM "tasks" ORDER BY ${sortBy};`;
   }
+  pool
+    .query(queryText)
+    .then((dbRes) => {
+      const dbRows = dbRes.rows;
+      console.table(dbRows);
+      res.send(dbRows);
+    })
+    .catch((err) => {
+      console.log(`GET ERROR: ${err}`);
+      res.sendStatus(500);
+    });
 });
 
 //  POST    --inserts new task to DB
